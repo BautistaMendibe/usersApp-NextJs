705b552bb4af12e319abf8b397b70235e60746bc
bauti <bautistamendibe10@gmail.com>
2024-01-25 17:29:58 -0300

    fetch de los datos de usuarios

--- app/page.js
+++ app/page.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
-function HomPage() {
+async function getUsers(){
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json();
+  return data.data;
+}
+
+async function HomPage() {
+  
+  const users = await getUsers();
+  
   return (
-    <div>HomPage</div>
+    <div>
+      {
+        <ul>
+          {
+            users.map((user) => {
+              return (
+                <li key={user.id}>
+                  <h5>{user.id} {user.first_name} {user.last_name}</h5>
+                  <p>email: {user.email}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      }
+    </div>
   )
 }
 
